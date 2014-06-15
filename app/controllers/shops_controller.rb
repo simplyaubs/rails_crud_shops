ae6049afc8f9a1d27d91d212afcba7b51a047f4b
Aubrey Howell <simplyaubs@gmail.com>
2014-06-15 08:30:49 -0700

    User can update a shop from list.

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -15,6 +15,21 @@ class ShopsController < ApplicationController
     end
   end
 
+  def show
+    @shop = Shop.find(params[:id])
+  end
+
+  def edit
+    @shop = Shop.find(params[:id])
+  end
+
+  def update
+    @shop = Shop.find(params[:id])
+    @shop.update_attributes!(shop_params)
+
+    redirect_to shops_path
+  end
+
   private
   def shop_params
     params.require(:shop).permit(:name, :specialty)
--- spec/features/shops_spec.rb
+++ spec/features/shops_spec.rb
@@ -11,4 +11,26 @@ feature 'CRUD shops' do
     expect(page).to have_content 'The Coffee Bean'
     expect(page).to have_content 'Coffee'
   end
+
+  scenario 'User can update a shop from list' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a shop'
+    fill_in 'Name', with: 'The Coffee Bean'
+    fill_in 'Specialty', with: 'Coffee'
+    click_on 'Add shop'
+    expect(page).to have_content 'The Coffee Bean'
+    expect(page).to have_content 'Coffee'
+    click_on 'The Coffee Bean'
+    expect(page).to have_content 'The Coffee Bean'
+    expect(page).to have_content 'Coffee'
+    click_on 'Edit'
+    fill_in 'Name', with: 'Burger King'
+    fill_in 'Specialty', with: 'Burgers'
+    click_on 'Update shop'
+    expect(page).to have_content 'Burger King'
+    expect(page).to have_content 'Burgers'
+    expect(page).to_not have_content 'The Coffee Bean'
+    expect(page).to_not have_content 'Coffee'
+  end
 end
